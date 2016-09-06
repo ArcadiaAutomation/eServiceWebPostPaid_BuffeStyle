a0f8a9ea046b9a9253c7b92483b5ec7890193bf2
sakon777 <sakon778@hotmail.com>
2016-09-06 17:01:16 +0700

    - add parameter out put directory

--- Library/Mobile/runPararell.py
+++ Library/Mobile/runPararell.py
@@ -14,14 +14,14 @@ import threading
 from functools import partial
 
 
-def call_script(run_dir, robot_test_suite, tag, lang, browser, run_name):
+def call_script(run_dir, robot_test_suite, robot_output, tag, lang, browser, run_name):
     print "in call script"
     # command = "robot -t " + run_name + " -d " + dir_path + " -o output" + run_name + " -r report" + run_name \
     #           + " -l log" + run_name + " -v ar_LANG:" + LANG + " -v ar_BROWSER:" + BROWSER + " -i " + tag + " " \
     #           + robot_test_suite
-    command = "robot -t " + run_name + " -d " + run_dir + " -o output" + run_name + lang + " -r report" + run_name + lang \
-              + " -l log" + run_name + lang + " -v ar_LANG:" + lang + " -v ar_BROWSER:" + browser + " -i " + tag + " " \
-                + robot_test_suite
+    command = "robot -t " + run_name + " -d " + robot_output + " -o output" + run_name + lang + " -r report" + run_name\
+              + lang + " -l log" + run_name + lang + " -v ar_LANG:" + lang + " -v ar_BROWSER:" + browser + " -i " + tag \
+              + " " + robot_test_suite
     print "command is a " + command
     os.system(command)
     # subp.call(args, shell=True)
@@ -30,7 +30,7 @@ def call_script(run_dir, robot_test_suite, tag, lang, browser, run_name):
     # thread.join()
 
 
-def parallel_execution(robot_path, robot_test_suite, tag="*", lang="EN", browser="gc"):
+def parallel_execution(robot_path, robot_test_suite, robot_output, tag="*", lang="EN", browser="gc"):
     print "begin"
     run_name = []
     path = robot_path + '\\' + robot_test_suite
@@ -51,7 +51,7 @@ def parallel_execution(robot_path, robot_test_suite, tag="*", lang="EN", browser
     run_dir = dir_path
     os.chdir(robot_path)
     pool = Pool(len(run_name))
-    func = partial(call_script, run_dir, robot_test_suite, tag, lang, browser)
+    func = partial(call_script, run_dir, robot_test_suite, robot_output, tag, lang, browser)
     pool.map_async(func, run_name)
     pool.close()
     pool.join()
@@ -78,12 +78,13 @@ if __name__ == "__main__":
     # parallel_execution("D:\ArcadiaAtlas\Robot\eServiceWebPostPaid_BuffeStyle", "eServiceWebPostPaidLogin.txt", "ptest")
     # parallel_execution("D:\ArcadiaAtlas\Robot\eServiceWebPostPaid_BuffeStyle", "eServiceWebPostPaidBillAISPostpaid.txt", "T1")
     print "[0] => " + sys.argv[0]
-    print "[1] => " + sys.argv[1]
-    print "[2] => " + sys.argv[2]
-    print "[3] => " + sys.argv[3]
-    print "[4] => " + sys.argv[4]
-    print "[5] => " + sys.argv[5]
-    parallel_execution(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    print "[1] => " + sys.argv[1]   # => robot path
+    print "[2] => " + sys.argv[2]   # => robot test suite
+    print "[3] => " + sys.argv[3]   # => robot output report
+    print "[4] => " + sys.argv[4]   # => tag
+    print "[5] => " + sys.argv[5]   # => lang
+    print "[6] => " + sys.argv[6]   # => browser
+    parallel_execution(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
 
 
 # def call_script(args):
