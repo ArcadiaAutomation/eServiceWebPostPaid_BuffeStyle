7410c901b826b4df8767bf8417b1273aa9af8d33
sakon777 <sakon778@hotmail.com>
2016-09-06 15:21:24 +0700

    - first version of run parallel after refactor

--- Library/Mobile/runPararell.py
+++ Library/Mobile/runPararell.py
@@ -13,69 +13,24 @@ import threading
 
 from functools import partial
 
-def f(x):
-    return x*x
 
-
-def copy_run():
-    p = Pool(5)
-    print(p.map(f, [1, 2, 3]))
-
-
-def call_script(run_dir, run_name):
+def call_script(run_dir, robot_test_suite, tag, lang, browser, run_name):
     print "in call script"
     # command = "robot -t " + run_name + " -d " + dir_path + " -o output" + run_name + " -r report" + run_name \
     #           + " -l log" + run_name + " -v ar_LANG:" + LANG + " -v ar_BROWSER:" + BROWSER + " -i " + tag + " " \
     #           + robot_test_suite
     command = "robot -t " + run_name + " -d " + run_dir + " -o output" + run_name + " -r report" + run_name \
-              + " -l log" + run_name + " -v ar_LANG:" + "EN" + " -v ar_BROWSER:" + "gc" + " -i " + "ptest" + " " \
-                + "eServiceWebPostPaidLogin.txt"
+              + " -l log" + run_name + " -v ar_LANG:" + lang + " -v ar_BROWSER:" + browser + " -i " + tag + " " \
+                + robot_test_suite
     print "command is a " + command
     os.system(command)
     # subp.call(args, shell=True)
     # thread = Thread(group=None, target=lambda: os.system(command))
     # thread.start()
     # thread.join()
-#
-#
-# def run(run_name):
-#     print "run_name : " + run_name
-#     # os.chdir(sys.argv[1])
-#     # os.system("start cmd /k robot -t "+run_name+" -o xml_"+run_name+".xml -l log_"+run_name+".html -r report_"+run_name+" eServiceWebPostPaidLogin.txt")
-#     # os.system("start cmd /k robot -t " + run_name + " -d D:\\ArcadiaAtlas\\" + run_name + " " + "-v ar_LANG:EN -v ar_BROWSER:gc " + sys.argv[2])
-#     # command = "robot -t " + run_name + " -d " + dir_path + " -o output" + run_name + " -r report" + run_name \
-#     #           + " -l log" + run_name + " -v ar_LANG:" + "EN" + " -v ar_BROWSER:" + "gc" + " -i " + "ptest" + " " \
-#     #           + "eServiceWebPostPaidLogin.txt"
-#     # os.system(command)
-#     # subp.check_call(command, shell=True)
-#     # thread = Thread(group=None, target=lambda: os.system(command))
-#     # thread.run()
-#     # # Later
-#     # if thread.is_alive():
-#     #     # Still running
-#     #     print('running...')
-#     # else:
-#     #     # Has finished
-#     #     print('running done...')
-#
-#     print('finish run...')
-#
-#
-# def test_copy(str1):
-#     print "Test Copy : Test1=> " + str1
-#
-#
-# def test_hello(str1, str2):
-#     print "Str1 = " + str1 + " Str2 = " + str2
-#
-#
-# def f(x):
-#     return x*x
-#
-#
 
 
-def parallel_execution(robot_path, robot_test_suite, tag="*", LANG="EN", BROWSER="gc"):
+def parallel_execution(robot_path, robot_test_suite, tag="*", lang="EN", browser="gc"):
     print "begin"
     run_name = []
     path = robot_path + '\\' + robot_test_suite
@@ -96,7 +51,7 @@ def parallel_execution(robot_path, robot_test_suite, tag="*", LANG="EN", BROWSER
     run_dir = dir_path
     os.chdir(robot_path)
     pool = Pool(len(run_name))
-    func = partial(call_script, run_dir)
+    func = partial(call_script, run_dir, robot_test_suite, tag, lang, browser)
     pool.map_async(func, run_name)
     pool.close()
     pool.join()
@@ -120,7 +75,16 @@ def parallel_execution(robot_path, robot_test_suite, tag="*", LANG="EN", BROWSER
 
 if __name__ == "__main__":
     # (robot_path, robot_test_suite, tag = "*", LANG = "EN", BROWSER = "gc")
-    parallel_execution("D:\ArcadiaAtlas\Robot\eServiceWebPostPaid_BuffeStyle", "eServiceWebPostPaidLogin.txt", "ptest")
+    # parallel_execution("D:\ArcadiaAtlas\Robot\eServiceWebPostPaid_BuffeStyle", "eServiceWebPostPaidLogin.txt", "ptest")
+    # parallel_execution("D:\ArcadiaAtlas\Robot\eServiceWebPostPaid_BuffeStyle", "eServiceWebPostPaidBillAISPostpaid.txt", "T1")
+    print "[0] => " + sys.argv[0]
+    print "[1] => " + sys.argv[1]
+    print "[2] => " + sys.argv[2]
+    print "[3] => " + sys.argv[3]
+    print "[4] => " + sys.argv[4]
+    print "[5] => " + sys.argv[5]
+    parallel_execution(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+
 
 # def call_script(args):
 #     subp.call(args, shell=True)
